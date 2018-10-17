27212dce219f3256f87f5430a433b5e775a4ae63
Stang <pmx42@live.cn>
2018-10-18 01:20:37 +0800

    Update curl version
    Adaptation for current condition

--- sustc-cas-login.sh
+++ sustc-cas-login.sh
@@ -7,12 +7,13 @@ then
 DATE=`date "+%Y-%m-%d %H:%M:%S"`
 URL=`echo "$URL" | sed 's/\r$//g'`
 echo "[$DATE] Logging..."
-FORM=`curl -L "${URL}" 2> /dev/null`
-ACTION=`echo "$FORM" | grep "<form " | sed 's/.*action="\(.*\)" .*/\1/'`
-LT=`echo "$FORM" | grep '<input type="hidden" name="lt" ' | sed 's/.*value="\(.*\)" .*/\1/'`
-EXEC=`echo "$FORM" | grep '<input type="hidden" name="execution" ' | sed 's/.*value="\(.*\)" .*/\1/'`
-DATA="username=$1&password=$2&lt=$LT&execution=$EXEC&_eventId=submit&submit=LOGIN"
-RESULT=`curl -L --data "$DATA" "http://weblogin.sustc.edu.cn$ACTION" 2> /dev/null`
+URL2="http://enet.10000.gd.cn:10001/sz/sz112/"`curl -L "${URL}" 2> /dev/null | grep "window.location" | sed -e 's/window.location = //' -e 's/;//' -e 's/\t//g' -e 's/["'\'']//g'`
+# https://stackoverflow.com/questions/35018899/using-curl-in-a-bash-script-and-getting-curl-3-illegal-characters-found-in-ur
+FORM=`curl -L "${URL2%$'\r'}" 2> /dev/null`
+# "\1"'s mean in sed: https://stackoverflow.com/questions/4609949/what-does-1-in-sed-do
+EXEC=`echo "${FORM}" | grep '<input type="hidden" name="execution" ' | sed -e 's/<input type="hidden" name="execution" value="\(.*\)".*/\1/' -e 's/\([^"]*\)"\/>.*/\1/' -e 's/ //g'`
+DATA="username=$1&password=$2&execution=$EXEC&_eventId=submit&geolocation="
+RESULT=`curl -L --data "${DATA}" "${URL2%$'\r'}" 2> /dev/null`
 if echo $RESULT | grep -q "<h2>success"
 then
 echo "Success"
